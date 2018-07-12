7b1afef995155898a903ab78f2fe2ae6203d703b
Unknown <mariotto.damien@gmail.com>
2018-07-12 20:23:28 +0100

    petite amélioration

--- view/topBar.php
+++ view/topBar.php
@@ -1,26 +1,19 @@
 <body class="container-fluid">
 <header>
     <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../controller/module.ctrl.php">StudentApp</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="">Modules</a></li>
-                    <li><a href="exams.ctrl.php">Exams</a></li>
-                    <li><a href="students.ctrl.php">Students</a></li>
-                    <li><a href="administration.ctrl.php">Administration</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="session_delete.ctrl.php"><span class="glyphicon glyphicon-user"></span> Sign out</a></li>
-                </ul>
-            </div>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="../controller/module.ctrl.php">StudentApp</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="">Modules</a></li>
+                <li><a href="exams.ctrl.php">Exams</a></li>
+                <li><a href="students.ctrl.php">Students</a></li>
+                <li><a href="administration.ctrl.php">Administration</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="session_delete.ctrl.php"><span class="glyphicon glyphicon-user"></span> Sign out</a></li>
+            </ul>
         </div>
     </nav>
 </header>
