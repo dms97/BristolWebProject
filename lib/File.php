ebc61e8268f12ed93173fa27c4d035b99438d81d
Lanastudio <lucas.lana@free.fr>
2018-07-16 16:51:32 +0100

    LLA - Correctif File.php

--- lib/File.php
+++ lib/File.php
@@ -1,17 +1,13 @@
 <?php
 
-class File
-{
-
-    public static function build_path($path_array)
-    {
+class File {
+    
+    public static function build_path($path_array) {
         // $path_array = array('controller','nom_fichier.php') ;
         $DS = DIRECTORY_SEPARATOR;
-
-        // $ROOT_FOLDER (sans slash à la fin) vaut "/home/ann2/votre_login/public_html/TD4" à l'IUT
+        $ROOT_FOLDER = __DIR__ . "/..";
         return $ROOT_FOLDER. $DS . join($DS, $path_array);
     }
-
 }
 
 ?>
