6d1f27909d1e3ce1f12efab0fb9008e6e769d6ad
Lanastudio <lucas.lana@free.fr>
2018-07-17 15:05:04 +0100

    LLA - scriptUser into ModelUser

--- model/ModelUser.php
+++ model/ModelUser.php
@@ -287,6 +287,31 @@ class ModelUser extends Model
             die();
         }
     }
+	
+	function addUser($id, $pwd, $fname, $lname, $mail, $role, $phone, $address) {
+		try {
+			$sql = 'INSERT INTO bristol.users VALUES (:id, :pwd, :fname, :lname, :mail, :role, :phone, :address)';
+			$verif = Model::$pdo->prepare($sql);
+
+			$values = array(
+							'id' => strip_tags($id),
+							'pwd' => strip_tags($pwd),
+							'mail' => strip_tags($fname),
+							'fname' => strip_tags($lname),
+							'lname' => strip_tags($mail),
+							'role' => strip_tags($role),
+							'phone' => strip_tags($phone),
+							'address' => strip_tags($address)
+						);
+
+			$verif->execute($values);
+			echo 'User added.';
+		} catch (PDOException $e) {
+			echo 'An error has occurred :/';
+			die();
+		}
+	}
+	
 }
 
 ?>
