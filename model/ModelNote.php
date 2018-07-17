8908b51b94e1e14c87366b2201b98c48ca02c45f
Unknown <mariotto.damien@gmail.com>
2018-07-17 15:53:12 +0100

    corec

--- model/ModelNote.php
+++ model/ModelNote.php
@@ -2,10 +2,11 @@
 
 require_once File::build_path(array('model','Model.php'));
 
-class ModelNote extends Model{
+class ModelNote extends Model
+{
 
     protected static $object = 'exammarks';
-    
+
     private $ModuleID;
     private $StudentID;
     private $Marks;
@@ -51,35 +52,37 @@ class ModelNote extends Model{
 
         return $tab_p;
     }
-    
-    public function getRange($StudentID) {
+
+    public function getRange($StudentID)
+    {
         $bdd = new Model();
         $sql = "SELECT Title ,Marks,ExamType,Ratio FROM `enrolled` INNER JOIN `users` ON users.Id=enrolled.StudentsID INNER JOIN `modules` ON modules.Id=enrolled.ModuleID INNER JOIN `examcomponents` ON examcomponents.ModuleId=enrolled.ModuleID INNER JOIN `exammarks` ON exammarks.ModuleID=enrolled.ModuleID AND exammarks.StudentsID=enrolled.StudentsID and exammarks.ExamComponentsID=examcomponents.Id WHERE users.id='$StudentID'";
         $result = $bdd->query($sql);
-        $notes=array();
-        $i=0;
-        if($result = mysqli_query($bdd, $sql)){
-            if(mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_array($result)){
+        $notes = array();
+        $i = 0;
+        if ($result = mysqli_query($bdd, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_array($result)) {
                     $notes[$i] = $row['Title'];
-                    $notes[$i+1] = $row['Marks'];
-                    $notes[$i+2] = $row['Ratio'];
-                    $i = $i +3;
+                    $notes[$i + 1] = $row['Marks'];
+                    $notes[$i + 2] = $row['Ratio'];
+                    $i = $i + 3;
                 }
                 // Free result set
                 mysqli_free_result($result);
-            } else{
+            } else {
                 echo "No records matching your query were found.";
             }
-        } else{
+        } else {
             echo "ERROR: Could not able to execute $sql. " . mysqli_error($bdd);
         }
         return $notes;
     }
 
-    public function addMark($moduleID,$StudentID,$mark,$ExamCompoId,$resit){
+    public function addMark($moduleID, $StudentID, $mark, $ExamCompoId, $resit)
+    {
         $bdd = new Model();
         $sql = "INSERT INTO `exammarks` (`ModuleID`, `StudentsID`, `Marks`, `ExamComponentsID`, `Resit`) VALUES (\''$moduleID'\', \''$StudentID'\', \''$mark'\', \''$ExamCompoId'\', \''$resit'\')";
     }
-    
+}
 ?>
