1d5b66695f7d6c26ed09b75799093b94405105a1
Lanastudio <lucas.lana@free.fr>
2018-07-17 15:49:12 +0100

    LLA - encore modif scriptUser

--- model/scriptUser.php
+++ model/scriptUser.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "../lib/File.php";
+require_once "ModelUser.php";
 require_once "Model.php";
 
 // test values
@@ -13,30 +14,7 @@ $role = 3;
 $phone = "0606060606";
 $adress = "somewhere";*/
 
-function addUser($id, $pwd, $fname, $lname, $mail, $role, $phone, $address) {
-	try {
-		$sql = 'INSERT INTO bristol.users VALUES (:id, :pwd, :fname, :lname, :mail, :role, :phone, :address)';
-		$verif = Model::$pdo->prepare($sql);
 
-		$values = array(
-						'id' => strip_tags($id),
-						'pwd' => strip_tags($pwd),
-						'mail' => strip_tags($fname),
-						'fname' => strip_tags($lname),
-						'lname' => strip_tags($mail),
-						'role' => strip_tags($role),
-						'phone' => strip_tags($phone),
-						'address' => strip_tags($address)
-					);
-
-		$verif->execute($values);
-		echo 'User added.';
-	} catch (PDOException $e) {
-		echo 'An error has occurred :/';
-		die();
-	}
-}
-
-addUser(100, "infres", "Bob", "MCLANE", "bob.mclane@yomail.com", 3, "0606060606", "somewhere");
+ModelUser::addUser(100, "infres", "Bob", "MCLANE", "bob.mclane@yomail.com", 3, "0606060606", "somewhere");
 
 ?>
