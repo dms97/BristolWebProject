9b0a1a2ec4227b1f8b65e5f15d688f636b0d8791
Unknown <mariotto.damien@gmail.com>
2018-07-18 12:11:53 +0100

    mise en forme du form module

--- view/module/addModule.php
+++ view/module/addModule.php
@@ -2,34 +2,44 @@
     <form method="post" action="index.php?controller=module&action=createModule">
         <fieldset>
             <legend>Module Title</legend>
-            <div class="form-group">
-                <label for="Title" class="col-sm-2 control-label">Module Title</label>
-                <input type="text" placeholder="Title" name="Title" id="Title" required/>
+            <div class="form-group row">
+                <label for="Title" class="col-sm-2 col-form-label">Module Title</label>
+                <div class="col-sm-10">
+                    <input type="text" placeholder="Title" name="Title" id="Title" required/>
+                </div>
             </div>
-            <div>
-                <label for="componentsNumber">Number of components</label>
-                <input type="number" id="componentsNumber" name="componentsNumber" required/>
+            <div class="form-group row">
+                <label for="componentsNumber" class="col-sm-2 col-form-label">Number of components</label>
+                <div class="col-sm-10">
+                    <input type="number" id="componentsNumber" name="componentsNumber" required/>
+                </div>
             </div>
         </fieldset>
         <fieldset>
             <legend>First component</legend>
-            <div>
-                <label for="examType" class="col-sm-2 control-label">Type of exam</label>
-                <select id="examType" name="examType" required>
-                    <option>Assignment</option>
-                    <option>Lab Tests</option>
-                    <option>Written Exam</option>
-                </select>
+            <div class="form-group row">
+                <label for="examType" class="col-sm-2 col-form-label">Type of exam</label>
+                <div class="col-sm-10">
+                    <select id="examType" name="examType" required>
+                        <option>Assignment</option>
+                        <option>Lab Tests</option>
+                        <option>Written Exam</option>
+                    </select>
+                </div>
             </div>
-            <div>
-                <label for="examType" class="col-sm-2 control-label">Type of exam</label>
-                <input type="date" id="examDate" name="examDate" value="<?php date("d/m/Y") ?>"
-                       min="<?php date("d/m/Y") ?>"
-                       max="31/12/2999"/>
+            <div class="form-group row">
+                <label for="examType" class="col-sm-2 col-form-label">Type of exam</label>
+                <div class="col-sm-10">
+                    <input type="date" id="examDate" name="examDate" value="<?php date("d/m/Y") ?>"
+                           min="<?php date("d/m/Y") ?>"
+                           max="31/12/2999"/>
+                </div>
             </div>
-            <div>
-                <label for="ratio" class="col-sm-2 control-label">Type of exam</label>
-                <input type="number" id="ratio" name="ratio" placeholder="100"/>
+            <div class="form-group row">
+                <label for="ratio" class="col-sm-2 col-form-label">Type of exam</label>
+                <div class="col-sm-10">
+                    <input type="number" id="ratio" name="ratio" placeholder="100"/>    
+                </div>
             </div>
         </fieldset>
     </form>
