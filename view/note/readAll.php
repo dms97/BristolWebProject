ec133774889ed90a57b9a5f336fd8096643b815c
Lanastudio <lucas.lana@free.fr>
2018-07-18 10:56:50 +0100

    LLA - Working NoteTable

--- view/note/readAll.php
+++ view/note/readAll.php
@@ -1,22 +1,27 @@
-<table id="exam_table">
+<table id="module_table">
     <tr>
         <th>Module</th>
         <th colspan="3">Assessment ans Exam Components</th>
         <th>Mean</th>
         <th>Grade</th>
     </tr>
-	<?php
-		foreach ($objet as $t) {
-			echo '<tr>'
-				. '<td>' . htmlspecialchars($t['exam']['titleModule']) .'</td>'
-				. '<td>' . htmlspecialchars($t['module']['exam1']['title']) . " : " . htmlspecialchars($t['module']['exam1']['coef']) .'</td>'
-				. '<td>' . htmlspecialchars($t['module']['exam2']['title']) . " : " . htmlspecialchars($t['module']['exam2']['coef']) .'</td>'
-				. '<td>' . htmlspecialchars($t['module']['exam3']['title']) . " : " . htmlspecialchars($t['module']['exam3']['coef']) .'</td>'
-				. '<td>' . htmlspecialchars($t['module']['mark']) . '</td>'
-				. '<td>' . htmlspecialchars($t['module']['grade']) . '</td>'
-				. '</tr>';
+    <?php
+	$tmp = "";
+	$i = 0;
+    foreach ($objet as $t) {
+        if ($tmp != htmlspecialchars($t->get("ModuleId"))) {
+			$tmp = htmlspecialchars($t->get("ModuleId"));
+			if ($i != 0) {
+				echo '</tr>';
+			}
+			echo '<tr>' . '<td><a href="' . "http://" . $adresse . $tp . "/index.php?controller=module&action=read&id=" . htmlspecialchars($t->get("ModuleId")) . '">' . htmlspecialchars($t->get("Title")) . '</a></td>';
 		}
+		echo '<td>' . htmlspecialchars($t->get("ExamType")) . " : " . htmlspecialchars($t->get("Marks")) . ' (' . htmlspecialchars($t->get("Ratio")) . '%)</td>';
+		$tmp = htmlspecialchars($t->get("ModuleId"));
+    }
+	echo '</tr>';
 	?>
+	
     <!-- Example:
 	<tr>
         <td>WP</td>
@@ -26,5 +31,5 @@
         <td>0</td>
         <td>A++</td>
     </tr>
-	--> 
+	-->
 </table>
