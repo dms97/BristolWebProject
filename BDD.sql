1cbe75002dd922f8fb90e9781761c0281bc17ea4
Alexis Jolin <snervis83@gmail.com>
2018-07-19 12:19:01 +0200

    AJout des user bdd

--- BDD.sql
+++ BDD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 18 Juillet 2018 à 10:44
+-- Généré le :  Jeu 19 Juillet 2018 à 12:18
 -- Version du serveur :  5.6.24
 -- Version de PHP :  5.6.8
 
@@ -31,6 +31,19 @@ CREATE TABLE IF NOT EXISTS `enrolled` (
   `StudentsID` int(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `enrolled`
+--
+
+INSERT INTO `enrolled` (`ModuleID`, `StudentsID`) VALUES
+  (1, 100),
+  (2, 100),
+  (4, 100),
+  (5, 100),
+  (1, 200),
+  (4, 200),
+  (5, 200);
+
 -- --------------------------------------------------------
 
 --
@@ -43,7 +56,23 @@ CREATE TABLE IF NOT EXISTS `examcomponents` (
   `ExamDate` date NOT NULL,
   `ExamType` varchar(20) NOT NULL,
   `Ratio` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `examcomponents`
+--
+
+INSERT INTO `examcomponents` (`Id`, `ModuleId`, `ExamDate`, `ExamType`, `Ratio`) VALUES
+  (1, 1, '2018-08-01', 'Assignment', 30),
+  (2, 1, '2018-08-01', 'Lab Test', 20),
+  (3, 1, '2018-08-01', 'Written Exam', 50),
+  (4, 2, '2018-08-01', 'Assignment', 50),
+  (5, 2, '2018-08-01', 'Lab Test', 50),
+  (6, 4, '2018-08-01', 'Assignment', 50),
+  (7, 4, '2018-08-01', 'Assignment', 50),
+  (8, 5, '2018-08-01', 'Assignment', 20),
+  (9, 5, '2018-08-01', 'Lab Test', 30),
+  (10, 5, '2018-08-01', 'Written Exam', 50);
 
 -- --------------------------------------------------------
 
@@ -59,6 +88,29 @@ CREATE TABLE IF NOT EXISTS `exammarks` (
   `Resit` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `exammarks`
+--
+
+INSERT INTO `exammarks` (`ModuleID`, `StudentsID`, `Marks`, `ExamComponentsID`, `Resit`) VALUES
+  (1, 100, 60, 1, 0),
+  (1, 100, 50, 2, 0),
+  (1, 100, 85, 3, 0),
+  (1, 200, 80, 1, 0),
+  (1, 200, 85, 2, 0),
+  (1, 200, 60, 3, 0),
+  (2, 100, 60, 4, 0),
+  (2, 100, 30, 5, 1),
+  (4, 100, 70, 6, 0),
+  (4, 100, 70, 7, 0),
+  (4, 200, 50, 4, 0),
+  (4, 200, 50, 5, 1),
+  (5, 100, 50, 8, 0),
+  (5, 100, 10, 9, 1),
+  (5, 100, 30, 10, 0),
+  (5, 200, 70, 6, 0),
+  (5, 200, 60, 7, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -68,7 +120,19 @@ CREATE TABLE IF NOT EXISTS `exammarks` (
 CREATE TABLE IF NOT EXISTS `modules` (
   `Id` int(16) NOT NULL,
   `Title` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `modules`
+--
+
+INSERT INTO `modules` (`Id`, `Title`) VALUES
+  (3, 'CMS'),
+  (4, 'LESPI'),
+  (2, 'WD'),
+  (5, 'WDF'),
+  (1, 'WP'),
+  (6, 'WT');
 
 -- --------------------------------------------------------
 
@@ -92,7 +156,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`Id`, `Password`, `FirstName`, `LastName`, `Email`, `Role`, `PhoneNumber`, `Address`) VALUES
-  (123456789, '2342B209B703BC94ABDC844229749A2433300FDC532013B86BAE1B3A38AEB580', 'test', 'test', 'test@yopmail.com', 1, '099999999', 'test');
+  (100, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'Alan', 'BOMBSKY', 'alan.bombsky@yomail.com', 3, '0606060606', 'somewhere'),
+  (200, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'Thierry', 'HUCHE', 'thierry.huche@yomail.com', 3, '0606060606', 'somewhere'),
+  (300, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'Diego', 'VEGA', 'diego.vega@yomail.com', 3, '0606060606', 'somewhere'),
+  (400, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'Bob', 'MCLANE', 'bob.mclane@yomail.com', 3, '0606060606', 'somewhere'),
+  (500, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'Odile', 'CROC', 'odile.croc@yomail.com', 2, '0606060606', 'somewhere'),
+  (600, '7361c5a7309ce4228173bda2a963106952146be401154d85bc64cb93431880b7', 'admin', 'ADMIN', 'admin.admin@yomail.com', 1, '0606060606', 'somewhere');
 
 --
 -- Index pour les tables exportées
@@ -136,12 +205,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `examcomponents`
 --
 ALTER TABLE `examcomponents`
-  MODIFY `Id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(16) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `modules`
 --
 ALTER TABLE `modules`
-  MODIFY `Id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(16) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
